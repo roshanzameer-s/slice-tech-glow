77831de686744bac86fea413ff773e86c651b56c
zameer11shaik <roshanzameer3490@gmail.com>
2025-07-04 10:36:57 +0530

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base:process.env.VITE_BASE_PATH || "/slice-tech-glow",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
